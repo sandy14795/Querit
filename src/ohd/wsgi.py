89715c8498e138d372277cad14fbb84a5321dac3
sandeep <sandeepsingh4545@gmail.com>
2016-04-29 01:26:50 +0530

    trying2

--- src/ohd/wsgi.py
+++ src/ohd/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ohd.settings")
 
-application = get_wsgi_application()
+
 
 try:
 	from dj_static import Cling
@@ -22,3 +22,6 @@ try:
 
 except:
 	pass	
+
+
+application = get_wsgi_application()	
